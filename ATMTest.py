77e0277ef24a08ca23aabe32ebe7df9b225501d7
zzx <519471465@qq.com>
2026-09-18 11:25:35 +0800

    6.增加一个ATM案例(新)

--- ATMTest.py
+++ ATMTest.py
@@ -1,25 +1,46 @@
 balance = 50000
-while True:
+def menu():
     print("---------主菜单-----------")
     print("1.查询余额")
     print("2.存款")
     print("3.取款")
     print("4.退出")
     choice = int(input("请输入你的选择："))
+    return choice
+
+def query():
+    print("你的余额为：", balance)
+
+def deposit():
+    global balance
+    amount = float(input("请输入存款金额："))
+    balance += amount
+    print("存款后余额为：", balance)
+    query()
+
+def withdraw():
+    global balance
+    amount = float(input("请输入取款金额："))
+    if amount > balance:
+        print("余额不足")
+    else:
+        balance -= amount
+        print("取款后余额为：", balance)
+        query()
+
+def exit():
+    print("退出系统")
+
+while True:
+    choice = menu()
     if choice == 1:
-        print("你的余额为：", balance)
+        query()
     elif choice == 2:
-        deposit = float(input("请输入存款金额："))
-        balance += deposit
-        print("存款后余额为：", balance)
+        deposit()
     elif choice == 3:
-        withdraw = float(input("请输入取款金额："))
-        if withdraw > balance:
-            print("余额不足")
-        else:
-            balance -= withdraw
-            print("取款后余额为：", balance)
+        withdraw()
     elif choice == 4:
+        exit()
         break
     else:
         print("输入错误")
